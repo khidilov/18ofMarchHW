4eb00e0a3e84073de5905821cf72c68a8155938f
khidilov <100746123+khidilov@users.noreply.github.com>
2022-04-10 13:13:56 +0400

    Add files via upload

---
+++ 18ofMarch_MotoredVehicleHW/18ofMarch_MotoredVehicleHW/Models/Car.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _18ofMarch_MotoredVehicleHW.Models
+{
+    internal class Car : MotoredVehicle
+    {
+        public string GetDetailedInfo(int FuelAmount)
+        {
+            return $"Make: {Make};\nModel: {Model};\nYear of manufaction: {ProductionYear};\nMotor Size: {MotorSize};\nColor {Color};\nFuel Amount {FuelAmount}:";
+        }
+        public bool isON = false;
+        public Car(string Make, string Model, string ProductionYear) : base(Make, Model, ProductionYear)
+        {
+
+        }
+
+        public Car()
+        {
+        }
+
+        public void TurnOn()
+        {
+            isON = true;
+        }
+        public void TurnOff()
+        {
+            isON = false;
+        }
+        public string Go(int Route, int FuelAmount, int FuelUsedPerKm)
+        {
+            if (isON == false) return "Maybe you should start the engine first";
+            else
+            {
+                if (FuelAmount > Route * FuelUsedPerKm)
+                {
+                    return $"You have reached your destination.\nRemaining fuel amount is: {FuelAmount = FuelAmount - Route * FuelUsedPerKm}";
+                }
+                else
+                {
+                    return $"No money, no honey. But you still can go {FuelAmount / FuelUsedPerKm}";
+                }
+            }
+
+        }
+    }
+}
---
+++ 18ofMarch_MotoredVehicleHW/18ofMarch_MotoredVehicleHW/Models/MotoredVehicle.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _18ofMarch_MotoredVehicleHW.Models
+{
+    internal class MotoredVehicle
+    {
+        public string Make;
+        public string Model;
+        public string ProductionYear;
+        public int MotorSize;
+        public int FuelTankSize;
+        public int FuelAmount;
+        public int FuelUsedPerKm;
+        public string Color;
+        public string Transmission;
+        public int HorsePower;
+        public int PassangerCount;
+        public MotoredVehicle()
+        {
+
+        }
+        public MotoredVehicle(string Make, string Model, string ProductionYear)
+        {
+            this.Make = Make;
+            this .Model = Model;
+            this .ProductionYear = ProductionYear;
+        }
+
+        public string GetInfo()
+        {
+            return $"Make: {Make}; \n Model: {Model}; \n Year of manufaction: {ProductionYear}";
+        } 
+        public string GetDetailedInfo()
+        {
+            return $"Make: {Make}; \n Model: {Model}; \n Year of manufaction: {ProductionYear}; \n Motor Size: {MotorSize}; \n Color {Color}:";
+        }
+    }
+}
